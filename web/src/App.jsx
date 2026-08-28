4594cd23bdaea59fcb74760fcebd19ea5bf83cc9
apolomoraes <vlogsapolo@gmail.com>
2026-08-28 13:19:57 -0300

    feat(web): fetch and display users list

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import GlobalStyle from './styles/global.js'
 import styled from 'styled-components'
 import {toast, ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useState, useEffect } from 'react'
+import Form from './components/Form.jsx'
+import Grid from './components/Grid.jsx'
+import axios from 'axios'
 
 const Container = styled.div`
   width: 100%;
@@ -13,11 +18,33 @@ const Container = styled.div`
   gap: 10px;
 `;
 
+const Title = styled.h2``;
+
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get('http://localhost:3000');
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
-      <Container></Container>
-      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form onEdit={onEdit}/>
+        <Grid users={users} />
+      </Container>
+      <ToastContainer autoClose={3000} position="bottom-right"/>
       <GlobalStyle />
     </>
   );
