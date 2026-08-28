261739de4f194a57fdaad5672c782509607fb5e5
apolomoraes <vlogsapolo@gmail.com>
2026-08-28 09:38:57 -0300

    feat(api): add user route and controller with get method

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ import 'dotenv/config';
 
 import express from "express";
 import cors from "cors";
+import usersRoutes from './routes/users.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/', usersRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
