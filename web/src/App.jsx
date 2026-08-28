4ee1a6f29e59be9513fcdf656e3f626bee089d38
apolomoraes <vlogsapolo@gmail.com>
2026-08-28 10:19:12 -0300

    feat(web): setup vite with react and initial app structure

---
+++ web/src/App.jsx
@@ -0,0 +1,26 @@
+import GlobalStyle from './styles/global.js'
+import styled from 'styled-components'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+function App() {
+  return (
+    <>
+      <Container></Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App
